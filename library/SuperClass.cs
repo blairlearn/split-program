f7967a2bed73a1bf630bb652a0dab0feab1aab7b
Blair Learn <blairl-nih@users.noreply.github.com>
2021-06-21 14:38:31 -0400

    Move the guts of 'main' to a separate project.

---
+++ library/SuperClass.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace split_program
+{
+    public class SuperClass
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
